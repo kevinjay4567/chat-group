fe778f03b5c5f0671d6f3d33e6db3c7af050cdd4
kevinjay4567 <kjayh@unal.edu.co>
2023-09-20 21:43:17 -0500

    first commit

---
+++ src/routes/chat/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Navbar from "./Navbar.svelte";
+</script>
+
+<div class="app">
+  <Navbar />
+  <main>
+    <slot></slot>
+  </main>
+</div>
+
+<style>
+  .app {
+    max-width: 100vw;
+    min-height: 100vh;
+    max-height: 100vh;
+    display: flex;
+    background-color: var(--secondary-color);
+  }
+
+</style>
---
+++ src/routes/chat/Navbar.svelte
@@ -0,0 +1,12 @@
+<nav class="navbar-container">
+  <ul>
+    <li>Channel 1</li>
+    <li>Channel 2</li>
+  </ul>
+</nav>
+
+<style>
+.navbar-container {
+  background-color: var(--primary-color);
+}
+</style>
